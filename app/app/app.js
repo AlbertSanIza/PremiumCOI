c7451fde34111038a7a0432206261410b32000ad
Albert Sanchez <albert.san.iza@gmail.com>
2015-03-11 06:49:18 -0800

    Added Material
    just playing around

--- app/app/app.js
+++ app/app/app.js
@@ -1,29 +1,16 @@
-var PremiumToolkit = angular.module('PremiumApp',['ngRoute','ui.bootstrap']);
+var premiumApp = angular.module('PremiumApp',['ngRoute','ui.bootstrap','PremiumControllers','ngMaterial']);
 
-PremiumToolkit.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/start', {
-        templateUrl: 'partials/start.html',
-        controller: 'CarouselDemoCtrl'
-      }).
-      otherwise({
-        redirectTo: '/start'
-      });
-  }]);
-
-PremiumToolkit.controller('CarouselDemoCtrl', function($scope){
-  $scope.myInterval = 5000;
-  var slides = $scope.slides = [];
-  $scope.addSlide = function() {
-    var newWidth = 600 + slides.length + 1;
-    slides.push({
-      image: 'http://placekitten.com/' + newWidth + '/300',
-      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+premiumApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/start', {
+      templateUrl: 'partials/start/start.html',
+      controller: 'CarouselDemoCtrl'
+    }).
+    when('/carousel', {
+      templateUrl: 'partials/carousel/carousel.html',
+      controller: 'CarouselDemoCtrl'
+    }).
+    otherwise({
+      redirectTo: '/start'
     });
-  };
-  for (var i=0; i<4; i++) {
-    $scope.addSlide();
-  }
-});
+}]);
---
+++ app/app/controllers.js
@@ -0,0 +1,21 @@
+var premiumControllers = angular.module('PremiumControllers',[]);
+
+premiumApp.controller('StartCtrl', function($scope){
+
+});
+
+premiumApp.controller('CarouselDemoCtrl', function($scope){
+  $scope.myInterval = 5000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: 'http://placekitten.com/' + newWidth + '/300',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }
+});
