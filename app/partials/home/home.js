d72bda5e43ff4f964ac23514571c3f7a63976b00
Albert Sanchez <albert.san.iza@gmail.com>
2015-03-16 06:24:48 -0700

    Combined firebase and card support
    its awesome!

--- app/partials/home/home.js
+++ app/partials/home/home.js
@@ -1,26 +1,24 @@
 var homeCtrl = angular.module('HomeCtrl',['ui.sortable', 'firebase']);
 
-homeCtrl.controller('HomeMain', function($scope){
-  $scope.items = [
-    {name: 'Perro'},
-    {name: 'Gato'}
-  ];
+homeCtrl.controller('HomeMain', function($scope, $firebaseObject){
+
+  var ref = new Firebase("https://vivid-inferno-387.firebaseio.com/");
+  var obj = $firebaseObject(ref);
+
+  obj.$loaded().then(function() {
+    $scope.kanbanBoard = obj;
+    obj.$bindTo($scope, "kanbanBoard");
+  });
+
+  $scope.kanbanBoard = {
+    "columns": [
+      {"name": "Task", "cards": []},
+      {"name": "User", "cards": []}
+    ]
+  };
+
   $scope.sortableOptions = {
-    containment: '#sortable-container'
+    containment: '#board'
   };
-});
 
-homeCtrl.controller('HomeFire', function($scope, $firebaseArray){
-  //var ref = new Firebase("https://vivid-inferno-387.firebaseio.com/");
-  //var ref = new Firebase("https://mz3y9nv2eru.firebaseio-demo.com/");
-  //$scope.messages = $firebaseArray(ref);
 });
-
-homeCtrl.directive('myDirective', function($compile) {
-  return {
-    restrict: 'E',
-    template: '<div><p data-ng-bind="item.name"></p></div>',
-    replace: true,
-  };
-});
-
